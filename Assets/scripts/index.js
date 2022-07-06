ba3eca22f8d0bd4ff3f0697de56b2ebf7decf275
Giovanni Nespoli <ggnespoli@gmail.com>
2022-07-06 16:51:16 -0300

    corra

--- Assets/scripts/index.js
+++ Assets/scripts/index.js
@@ -1,12 +1,29 @@
 function profileConfig() {
 
     var display = document.getElementById('profileNavigation').style.display || 'none';
+    
+    const animation = document.getElementById('animation');
 
     if (display === 'none') {
         document.getElementById('profileNavigation').style.display = 'block';
+        animation.style.transform = 'rotate(180deg)';
+        animation.style.transition = '0.5s';
     } else {
         document.getElementById('profileNavigation').style.display = 'none';
+        animation.style.transform = 'rotate(0deg)';
     }
 
+
+}
+
+function openClose() {
+
+    const btn = document.getElementsByClassName('openClose').style.display || 'block';
+
+    const openClose = document.getElementsByClassName('openClose');
+
+    if (btn === 'block') {
+        openClose.style.width = '1000px'
+    }
 
 }
