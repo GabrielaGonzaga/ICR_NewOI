8469ee649ba327372a51997713382ad50c6c41e4
Giovanni Nespoli <ggnespoli@gmail.com>
2022-07-07 13:25:37 -0300

    dashboard

--- Assets/scripts/index.js
+++ Assets/scripts/index.js
@@ -1,7 +1,7 @@
 function profileConfig() {
 
     var display = document.getElementById('profileNavigation').style.display || 'none';
-    
+
     const animation = document.getElementById('animation');
 
     if (display === 'none') {
@@ -16,14 +16,41 @@ function profileConfig() {
 
 }
 
-function openClose() {
+function openCloseNotifications() {
 
-    const btn = document.getElementsByClassName('openClose').style.display || 'block';
+    const select = document.getElementById('selectNotification');
 
-    const openClose = document.getElementsByClassName('openClose');
+    const btn = document.getElementById('notificationBtn');
+    const path = document.getElementById('path');
+    const path2 = document.getElementById('path2');
 
-    if (btn === 'block') {
-        openClose.style.width = '1000px'
+    const text = document.getElementById('nav-text');
+
+    var display = select.style.display || 'none'
+
+    if (display === 'none') {
+
+        select.style.display = 'flex';
+
+        btn.style.transform = 'rotate(180deg)';
+        btn.style.transition = '0.5s';
+        btn.style.color = '#233E71';
+
+        path.style.color = '#233E71';
+        path2.style.color = '#233E71';
+        
+        text.style.color= '#233E71';
+
+    } else if ( display === 'flex') {
+
+        select.style.display = 'none';
+        btn.style.transform = 'rotate(0deg)';
+        btn.style.transition = '0.5s';
+        btn.style.color = '#A6A6A6';
+
+        path.style.color = '#a6a6a6';
+        path2.style.color = '#a6a6a6';
+
+        text.style.color= '#a6a6a6';
     }
-
 }
